0373629d6ac16116bb94455f4c3c2f3411baf9fa
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2024-08-25 18:50:10 +0800

    cleanup

--- apifun/d2e-functions/etc/_fastifytest/service.ts
+++
@@ -1,7 +0,0 @@
-export class Service {
-
-	async get(req, reply) {
-		console.log("get", req.url);
-		return { key: "valsue" };
-	}
-}
--- apifun/d2e-functions/etc/_pet2/index.ts
+++
@@ -1,8 +0,0 @@
-import Fastify from "npm:fastify@v5.0.0-alpha.2";
-const fastify = Fastify({ logger: false });
-
-fastify.get("/", (request, reply) => {
-  reply.send("Hello world!");
-});
-
-fastify.listen({ port: 3000 });
