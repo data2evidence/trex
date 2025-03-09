4f21fee1c0c09547519d17bd1f20fe1a66da45c7
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-03-09 22:52:56 +0800

    fix build

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-RUST_BACKTRACE=1 LLAMA_CPP_SYS_NO_BUILD_INFO=1 CARGO_NET_GIT_FETCH_WITH_CLI=true cargo clippy --workspace --all-targets --all-features -- -D warnings -D clippy::unnecessary-literal-unwrap
+cargo clippy --workspace --all-targets --all-features -- -D warnings -D clippy::unnecessary-literal-unwrap
