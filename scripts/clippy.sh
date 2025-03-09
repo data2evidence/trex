7f3c9288b4d02dff8dd71f371f890e3fa5dd59d6
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-03-10 00:28:04 +0800

    fix

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cargo clippy --workspace --all-targets --all-features -- -D warnings -D clippy::unnecessary-literal-unwrap
+(cargo build --workspace --all-targets --all-features -- -D warnings -D clippy::unnecessary-literal-unwrap || true) && cargo clippy --workspace --all-targets --all-features -- -D warnings -D clippy::unnecessary-literal-unwrap
