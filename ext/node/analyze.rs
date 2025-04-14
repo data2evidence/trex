a30087aebe407a09866aab7e65b3b15ccaa94c72
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-04-14 11:05:12 +0800

    fix fmt

--- ext/node/analyze.rs
+++ ext/node/analyze.rs
@@ -154,7 +154,7 @@ impl<TCjsCodeAnalyzer: CjsCodeAnalyzer> NodeCodeTranslator<TCjsCodeAnalyzer> {
         let translated_source = source.join("\n");
         Ok(translated_source)
     }
-    
+
     #[allow(clippy::needless_lifetimes)]
     async fn analyze_reexports<'a>(
         &'a self,
