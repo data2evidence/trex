c00a58eec790aeccd9dfb154a0fe8caa7562577e
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-05-02 11:10:47 +0800

    limit copy without replication

--- ext/trex/js/trex_lib.js
+++ ext/trex/js/trex_lib.js
@@ -89,15 +89,18 @@ export class DatabaseManager {
 					}
 				}
 			} else {
-				console.log(`TREX NO PUB FOUND ${c.id}`)
-				const key = `${c.id}`
-				if(!(key in this.getPublications)) {
-					this.#add_postgres(`${key}_pg`, {host: c.host, port: c.port, databaseName: c.name, user: adminCredentials.username, password: adminCredentials.password});
-					const res = JSON.parse(op_execute_query(`${key}_pg`,"select table_schema as schema,table_name as name from information_schema.tables where table_type = 'BASE TABLE' and table_schema not in ('information_schema','pg_catalog') ", []));
-					op_copy_tables(res, key, c.host, c.port, c.name, adminCredentials.username, adminCredentials.password);
-					const pub = this.getPublications();
-					pub[key] = true;
-					this.#setPublications(pub);
+				if (c.vocabSchemas && c.vocabSchemas.length > 0) {
+					console.log(`TREX NO PUB FOUND ${c.id}`)
+					const key = `${c.id}`
+					if(!(key in this.getPublications) {
+						this.#add_postgres(`${key}_pg`, {host: c.host, port: c.port, databaseName: c.name, user: adminCredentials.username, password: adminCredentials.password});
+						const schemas = c.vocabSchemas.map(x => `'${x}'`).join(",");
+						const res = JSON.parse(op_execute_query(`${key}_pg`,`select table_schema as schema,table_name as name from information_schema.tables where table_type = 'BASE TABLE' and table_schema in (${schemas})`, []));
+						op_copy_tables(res, key, c.host, c.port, c.name, adminCredentials.username, adminCredentials.password);
+						const pub = this.getPublications();
+						pub[key] = true;
+						this.#setPublications(pub);
+					}
 				}
 			}
 		}
